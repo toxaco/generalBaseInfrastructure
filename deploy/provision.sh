2117588f97b3ab13473946b5a87fd4775bdd8693
Rafael de Souza <rafael_souza31@hotmail.com>
2018-06-28 09:11:51 +0100

    test..

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-export SYMFONY__DATABASE__USER="just another simple user"
-
-composer install -o --no-interaction
+#export SYMFONY__DATABASE__USER="just another simple user"
+#composer install -o --no-interaction
 
 service nginx restart
