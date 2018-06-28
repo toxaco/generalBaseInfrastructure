d2a46dcb561adf5efc7e7e5bf28894f41b23703b
Rafael de Souza <rafael_souza31@hotmail.com>
2018-06-28 09:53:11 +0100

    test..

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,6 +21,7 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
+
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
